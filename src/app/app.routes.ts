dada1c41ff0d1b7cbeb5a809ae98d0ce7e80d5fc
OdeStaples <prabhuprasad95@gmail.com>
2025-06-03 22:50:21 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./containers/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'movie/:id',
+    loadComponent: () =>
+      import('./containers/movie-detail/movie-detail.component').then(
+        (c) => c.MovieDetailComponent
+      ),
+  },
+  { path: '**', redirectTo: '/home' },
+];
