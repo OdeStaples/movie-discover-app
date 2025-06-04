13c1c904b36a185499ea4d1ee198ba4588ade34a
OdeStaples <prabhuprasad95@gmail.com>
2025-06-04 19:47:31 +0530

    fix: Update image paths to use relative URLs for better compatibility

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -57,7 +57,9 @@ export class MovieService {
   }
 
   getImageUrl(path: string, size: string = 'w500'): string {
-    return path ? `https://image.tmdb.org/t/p/${size}${path}` : '/no-image.png';
+    return path
+      ? `https://image.tmdb.org/t/p/${size}${path}`
+      : './no-image.png';
   }
 
   getBackdropUrl(path: string): string {
